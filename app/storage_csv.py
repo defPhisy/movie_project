0330c275913cb9cec5bf68b238e83eb27131caac
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-10-30 07:38:40 +0100

    add StorageCsv and movie_db.csv

---
+++ app/storage_csv.py
@@ -0,0 +1,66 @@
+import csv
+from istorage import IStorage
+from istorage import TITLE, YEAR, RATING
+
+
+class StorageCsv(IStorage):
+    def __init__(self, file_path) -> None:
+        self.file_path = file_path
+
+    def get_movie_data(self) -> list[dict]:
+        with open(self.file_path, 'r') as file:
+            dict_reader = csv.DictReader(file)
+            data = dict_reader
+        return list(data)
+
+    def _list_movies(self) -> dict[str, dict]:
+        movies = self.get_movie_data()
+        movie_dict = {}
+        for movie in movies:
+            movie_dict[movie[TITLE]] = {
+                "rating": movie[RATING],
+                "year": movie[YEAR],
+            }
+        return movie_dict
+
+    def _add_movie(self, title, year, rating, poster="placeholder") -> None:
+        movies = self.get_movie_data()
+        movies.append({
+            "Title": title,
+            "Rating": rating,
+            "Year": year,
+            "poster": poster,
+        })
+
+        self._save_movies(movies)
+
+    def _delete_movie(self, title):
+        movies = self.get_movie_data()
+        for index, movie in enumerate(movies):
+            if movie[TITLE].lower() == title.lower():
+                movies.remove(movies[index])
+
+        self._save_movies(movies)
+
+    def _update_movie(self, title, rating) -> None:
+        movies = self.get_movie_data()
+        for index, movie in enumerate(movies):
+            if movie[TITLE].lower() == title.lower():
+                movies[index]["Rating"] = rating
+
+        self._save_movies(movies)
+
+    def _save_movies(self, movies) -> None:
+        """Save movies in json file.
+
+        Arguments:
+            movies -- dictionary of all movies
+        """
+
+        field_names = movies[0].keys() 
+        movies = self.get_movie_data()
+        
+        with open(self.file_path, 'w') as file: 
+            writer = csv.DictWriter(file, fieldnames = field_names) 
+            writer.writeheader() 
+            writer.writerows(movies) 
